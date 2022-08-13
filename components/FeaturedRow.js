aa4e0e12dadef68899205fce629f06b496223929
David Benedette <davidbenedette@gmail.com>
2022-08-13 11:08:54 -0300

    Restoration Project with bug two mocks

---
+++ components/FeaturedRow.js
@@ -0,0 +1,64 @@
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
+import sanityClient, {urlFor} from "../sanity";
+
+const FeaturedRow = ({id, title, description}) => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() =>{
+    sanityClient.fetch(`
+      *[_type == "featured" && _id == $id] {
+        ...,
+        restaurants[]->{
+          ...,
+          dishes[]->,
+          type-> {
+            name
+          }
+        },
+      }[0]
+    `, { id }
+    ).then((data) => {
+      setRestaurants(data?.restaurants);
+    });
+  },[]);
+
+  return (
+    <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB"/>
+      </View>
+
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+
+      <ScrollView
+        horizontalcontentContainerStyle={{
+            paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4" 
+      >
+          {restaurants?.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant._id}
+              id={restaurant._id}
+              imgUrl={restaurant.Image}
+              title={restaurant.name}
+              dishes={restaurant.dishes}
+              genre={restaurant.type?.name}
+              address={restaurant.address}
+              rating={restaurant.rating}
+              short_description={restaurant.short_description}
+              long={restaurant.long}
+              lat={restaurant.lat}
+            />
+          ))}
+      </ScrollView>
+    </View>
+  )
+}
+
+export default FeaturedRow
